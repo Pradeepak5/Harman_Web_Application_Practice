b92ea0fcd56ba5f68ce3ace3216ba50730f9a572
Pradeepak5 <pradeepak571@gmail.com>
2022-04-04 10:10:43 +0530

    home gallery contact pages added

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def Welcome():
     return "Welcome to my website"
 
+@app.route("/contact")
+def Contact():
+    return "Contact Page"
+
+@app.route("/gallery")
+def Gallery():
+    return "Gallery Page"
+
+@app.route("/home")
+def Home():
+    return "Home Page"
+
 if __name__=="__main__":
     app.run()
